fa486274598520401ecf39b8873399c103148e60
mtaner <mervetaner88@gmail.com>
2016-06-26 17:03:11 +0100

    Added methods to keep score of sets and games with respective tests

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -8,12 +8,74 @@ describe('Bowling', function() {
 		game = new Bowling();
 	});
 
-	describe('#play', function(){
+	describe('#playRoundOne', function(){
 		it('returns a random number for pins hit', function(){
 			spyOn(Math, 'floor').and.returnValue(4);
-			expect(game.play()).toEqual(5)
+			expect(game.playRoundOne()).toEqual(5);
 		});
 	});
 
-	
+	describe('#playRoundTwo', function() {
+		it('allows the return of a random number that is left over from play round one', function() {
+			spyOn(Math, 'floor').and.returnValue(4);
+			game.playRoundOne();
+			game.playRoundTwo();
+			expect(game.roundTwoPins).toEqual([1,2,3,4,5])
+		});
+
+		it('returns a random number from the roundTwoPins array', function(){
+			spyOn(Math, 'floor').and.returnValue(3);
+			game.playRoundOne();
+			spyOn(Math, 'floor').and.returnValue(4);
+			expect(game.playRoundTwo()).toEqual(5);
+		});
+
+		it('returns 0 when player strikes', function(){
+			spyOn(Math, 'floor').and.returnValue(9)
+			game.playRoundOne();
+			expect(game.playRoundTwo()).toEqual(0);
+		});
+
+
+	});
+
+	describe("#resetSetScore", function(){
+		it('empties set_score array after round two', function(){
+			game.playRoundOne();
+			game.playRoundTwo();
+			expect(game.set_score).toEqual([])
+		});
+	});
+
+	describe('#addSetScore', function() {
+		it('keep scores for round 1 and 2 for every set of the game', function(){
+			spyOn(Math, 'floor').and.returnValue(4);
+			game.playRoundOne();
+			expect(game.set_score).toEqual([5]);
+		});
+
+	});
+
+
+	describe('#gameScore', function() {
+		it('stores the set scores', function() {
+			game.playRoundOne();
+			game.playRoundTwo();
+			expect(game.game_score.length).toBe(1)
+		});
+
+
+	})
+
+
+
+	// describe('#setOfGame'), function() {
+	// 	it('knows the which set player is in', function(){
+	//
+	//
+	// 	});
+	//
+	// };
+
+
 });
--- src/bowling.js
+++ src/bowling.js
@@ -1,10 +1,52 @@
-function Bowling () {
+function Bowling() {
+	// this.game_score = [];
+	this.set_score = [];
+	this.game_score = [];
 };
 
 Bowling.prototype = {
-	play: function() {
+	playRoundOne: function() {
 		var pinsKnocked = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-		var result = pinsKnocked[Math.floor(Math.random() * pinsKnocked.length)];
-		return result;
+		this.result = pinsKnocked[Math.floor(Math.random() * pinsKnocked.length)];
+		this.addSetScore();
+		return this.result;
+	},
+
+	playRoundTwo: function() {
+		if(this.result === 10) {
+			this.result = 0;
+		} else {
+			this.roundTwoPins = [];
+			for (var i = 1; i <= (10-this.result); i++) {
+			this.roundTwoPins.push(i);
+			this.result = this.roundTwoPins[Math.floor(Math.random() * this.roundTwoPins.length)];
+			}
+		};
+		this.addSetScore();
+		this.gameScore();
+		this.resetSetScore();
+		return this.result;
+	},
+
+	addSetScore: function(){
+		this.set_score.push(this.result);
+		return this.set_score;
+	},
+
+	gameScore: function(){
+		this.game_score.push(this.set_score);
+		return this.game_score;
+	},
+
+	resetSetScore: function() {
+		this.set_score = [];
 	}
 };
+
+// game = new Bowling();
+// console.log(game.playRoundOne());
+// console.log(game.playRoundTwo());
+//
+// console.log(game.set_score);
+// game.addSetScore();
+// console.log(game.set_score);
